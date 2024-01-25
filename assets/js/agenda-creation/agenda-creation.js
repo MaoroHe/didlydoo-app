8f58c4214eb1d58c3e6a1984e682d7cda70b3a67
Seaandsurf <mathias.hardat@gmail.com>
2024-01-25 14:48:55 +0100

    push poure le merge

--- assets/js/agenda-creation/agenda-creation.js
+++ assets/js/agenda-creation/agenda-creation.js
@@ -15,7 +15,11 @@ export function displayEvents(data) {
     data.forEach((event) => {
         const eventDiv = document.createElement("div");
         eventDiv.classList.add('eventBox');
-        eventDiv.innerHTML = `<h2 class="events__title">${event.name}</h2><p class="events__description">${event.description}</p><p class="events__author">${event.author}</p><button class="events__edit-button edit-button" data-id="${event.id}">Edit Event</button><button class="events__delete-button delete-button" data-id="${event.id}">Delete Event</button>`;
+        eventDiv.innerHTML = `<h2 class="events__title">${event.name}</h2>
+        <p class="events__description">${event.description}</p>
+        <p class="events__author">${event.author}</p>
+        <button class="events__edit-button edit-button" data-id="${event.id}">Edit Event</button>
+        <button class="events__delete-button delete-button" data-id="${event.id}">Delete Event</button>`;
         const table = document.createElement("table");
         const thead = document.createElement("thead");
         const tbody = document.createElement("tbody");
@@ -25,6 +29,7 @@ export function displayEvents(data) {
         tr.appendChild(th);
         event.dates.forEach((date) => {
             const th = document.createElement("th");
+
             th.textContent = date.date;
             tr.appendChild(th);
         });
@@ -34,7 +39,7 @@ export function displayEvents(data) {
             const tr = document.createElement("tr");
             const td = document.createElement("td");
             td.textContent = attendee.name;
-            if(!nameArray.includes(attendee.name)){
+            if (!nameArray.includes(attendee.name)) {
                 nameArray.push(attendee.name);
             }
             tr.appendChild(td);
@@ -48,7 +53,7 @@ export function displayEvents(data) {
         table.appendChild(tbody);
         eventDiv.appendChild(table);
         allEvents.appendChild(eventDiv);
-
+        // DELETE BUTON ------------------
         const deleteButton = eventDiv.querySelector(".delete-button");
         deleteButton.addEventListener("click", () => {
             const eventId = deleteButton.getAttribute("data-id");
@@ -71,36 +76,36 @@ export function displayEvents(data) {
             checkbox.name = `${apiDate.date}`;
             checkbox.addEventListener("change", () => {
                 const participantName = nameInput.value;
-                if(nameArray.includes(participantName)){
-                    for(let i=0; i<updatedData.length; i++){
-                        if(updatedData[i].id === event.id){
-                            if(updatedData[i].name === participantName){
-                                if(updatedData[i].date === apiDate.date){
-                                    if(checkbox.checked){
+                if (nameArray.includes(participantName)) {
+                    for (let i = 0; i < updatedData.length; i++) {
+                        if (updatedData[i].id === event.id) {
+                            if (updatedData[i].name === participantName) {
+                                if (updatedData[i].date === apiDate.date) {
+                                    if (checkbox.checked) {
                                         updatedData[i].available = true;
-                                    }else{
+                                    } else {
                                         updatedData[i].available = false;
                                     }
                                 }
                             }
                         }
                     }
-                }else{
-                    if(checkbox.checked){
-                        newData.push({date: apiDate.date, available: true})
-                    }else{
-                        newData.push({date: apiDate.date, available: false})
+                } else {
+                    if (checkbox.checked) {
+                        newData.push({ date: apiDate.date, available: true })
+                    } else {
+                        newData.push({ date: apiDate.date, available: false })
                     }
                 }
             });
             const label = document.createElement("label");
             label.setAttribute("for", `checkbox ${idCb}`);
             label.textContent = apiDate.date;
-            
+
             checkboxDiv.appendChild(checkbox);
             checkboxDiv.appendChild(label);
             apiDate.attendees.forEach(attendee => {
-                updatedData.push({id:event.id, name: attendee.name, date: apiDate.date, available: attendee.available});
+                updatedData.push({ id: event.id, name: attendee.name, date: apiDate.date, available: attendee.available });
             });
             idCb++;
         });
@@ -128,88 +133,88 @@ export function displayEvents(data) {
 function updateParticipantAvailability(eventId, participantName) {
     // let id = 0;
     let dataToAdd = [];
-    if(nameArray.includes(participantName)){
+    if (nameArray.includes(participantName)) {
         updatedData.forEach(data => {
-            if(data.id === eventId){
-                if(data.name === participantName){
-                    dataToAdd.push({date: data.date, available: data.available });
+            if (data.id === eventId) {
+                if (data.name === participantName) {
+                    dataToAdd.push({ date: data.date, available: data.available });
                 }
             }
         });
         fetch("http://localhost:3000/api/events/")
-        .then((response) => response.json())
-        .then((json) => {
-            json.forEach(obj => {
-                if(obj.id === eventId){
-                    obj.dates.forEach(apiDate => {
-                        apiDate.attendees.forEach(attend => {
-                            if(attend.name === participantName){
-                                // console.log(eventId, participantName);
-                                fetch(`http://localhost:3000/api/events/${eventId}/attend`, {
-                                    method: "PATCH",
-                                    headers: {
-                                        "Content-Type": "application/json"
-                                    },
-                                    body: JSON.stringify({name: participantName, dates: dataToAdd})
-                                })
-                                .then((response) => response.json())
-                                .then((json) => {
-                                    console.log(json);
-                                })
-                                .catch((error) => {
-                                    console.error(
-                                        "Erreur lors de la mise à jour de la disponibilité du participant :", error
-                                    );
-                                });
-                            }
+            .then((response) => response.json())
+            .then((json) => {
+                json.forEach(obj => {
+                    if (obj.id === eventId) {
+                        obj.dates.forEach(apiDate => {
+                            apiDate.attendees.forEach(attend => {
+                                if (attend.name === participantName) {
+                                    // console.log(eventId, participantName);
+                                    fetch(`http://localhost:3000/api/events/${eventId}/attend`, {
+                                        method: "PATCH",
+                                        headers: {
+                                            "Content-Type": "application/json"
+                                        },
+                                        body: JSON.stringify({ name: participantName, dates: dataToAdd })
+                                    })
+                                        .then((response) => response.json())
+                                        .then((json) => {
+                                            console.log(json);
+                                        })
+                                        .catch((error) => {
+                                            console.error(
+                                                "Erreur lors de la mise à jour de la disponibilité du participant :", error
+                                            );
+                                        });
+                                }
+                            });
                         });
-                    });
-                }
-                // else{
-                //     console.log("ko");
-                // }
+                    }
+                    // else{
+                    //     console.log("ko");
+                    // }
+                });
             });
-        });
-    }else{
+    } else {
         fetch("http://localhost:3000/api/events/")
-        .then((response) => response.json())
-        .then((json) => {
-            json.forEach(obj => {
-                if(obj.id === eventId){
-                    fetch(`http://localhost:3000/api/events/${eventId}/attend`, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({ name: participantName, dates: newData})
-                    })
-                    .then((response) => response.json())
-                    .then((json) => {
-                        console.log(json);
-                    })
-                    .catch((error) => {
-                        console.error(
-                            "Erreur lors de la mise à jour de la disponibilité du participant :", error
-                        );
-                    });
-                }
+            .then((response) => response.json())
+            .then((json) => {
+                json.forEach(obj => {
+                    if (obj.id === eventId) {
+                        fetch(`http://localhost:3000/api/events/${eventId}/attend`, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({ name: participantName, dates: newData })
+                        })
+                            .then((response) => response.json())
+                            .then((json) => {
+                                console.log(json);
+                            })
+                            .catch((error) => {
+                                console.error(
+                                    "Erreur lors de la mise à jour de la disponibilité du participant :", error
+                                );
+                            });
+                    }
+                });
             });
-        });
         nameArray.push(participantName);
         // updatedData.push({name: participantName, date})
         fetch("http://localhost:3000/api/events/")
-        .then((response) => response.json())
-        .then((json) => {
-            json.forEach(event => {
-                event.dates.forEach(apiDate => {
-                    apiDate.attendees.forEach(attendee => {
-                        if(attendee.name === participantName){
-                            updatedData.push({id:event.id, name: attendee.name, date: apiDate.date, available: attendee.available});
-                        }
+            .then((response) => response.json())
+            .then((json) => {
+                json.forEach(event => {
+                    event.dates.forEach(apiDate => {
+                        apiDate.attendees.forEach(attendee => {
+                            if (attendee.name === participantName) {
+                                updatedData.push({ id: event.id, name: attendee.name, date: apiDate.date, available: attendee.available });
+                            }
+                        });
                     });
                 });
             });
-        });
     }
     location.reload();
 }
