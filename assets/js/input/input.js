f6f87fe24b05a28638f349aa879f55ccadc3c4c7
MaoroHe <maorohecq1409@gmail.com>
2024-01-25 14:44:26 +0100

    darkmode function + fix some basic issue

--- assets/js/input/input.js
+++ assets/js/input/input.js
@@ -1,14 +1,11 @@
 import { dateAdd } from "../dateAdd/dateAdd.js";
-import { postToApi } from "../post/post.js";
 
 export let modalClick = () => {
     const addBtn = document.querySelector('.addBtn');
-    const btnSub = document.getElementById('eventSubmit');
     const addDate = document.getElementById('addDate');
     const close = document.getElementById('close');
     const boite = document.getElementById('boite');
     const dateVal = document.getElementById('eventDate')
-    const maine = document.getElementById('events')
 
     addBtn.addEventListener('click', (event) => {
         boite.style.display = "block";
@@ -22,12 +19,6 @@ export let modalClick = () => {
         if (event.target == boite) {
             boite.style.display = "none";
           }
-    })
-
-    btnSub.addEventListener('click', (event) => {
-        event.preventDefault();
-        postToApi();
-        
     })
 
     addDate.addEventListener('click', (event)=> {
